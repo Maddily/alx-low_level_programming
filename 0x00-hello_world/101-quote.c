d5f324d41c1eedf9af2386be96e892e47584e56b
Maddily <115709272+Maddily@users.noreply.github.com>
2023-08-10 14:12:07 +0000

    Edit syntax to change string length

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
+#define STRING \
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+
 /**
   * main - Prints a string to the standard error
   * Return: Returns 1
   */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	fputs(STRING, stderr);
 	return (1);
 }
