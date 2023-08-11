fe70000fb12888be84582f8f7f492f4ef2bff06f
Maddily <115709272+Maddily@users.noreply.github.com>
2023-08-11 12:32:30 +0000

    Print the numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the numbers from 0 to 9
+  * Return: Returns 0
+  */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
