5cc6f2f798ff2c977770572e156139dae6fdeed9
Maddie <115709272+Maddily@users.noreply.github.com>
2023-08-15 21:22:42 +0300

    Correct mistakes

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,14 +6,13 @@
  */
 int main(void)
 {
-	int i, sum;
+	int i, sum = 0;
 
 	for (i = 3; i < 1024; i++)
 	{
-		if (i % 3 || i % 5)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
 			sum += i;
-			i++;
 		}
 	}
 	printf("%d\n", sum);
