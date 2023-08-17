85364461a072ab7fde6d2bd441b00fb5c0112157
Maddie <115709272+Maddily@users.noreply.github.com>
2023-08-17 20:04:59 +0300

    Use unsigned int data type

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,23 +6,17 @@
  */
 void print_number(int n)
 {
+	unsigned int number = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		number = -number;
 	}
-
-	if (n < 10)
+	if (number >= 10)
 	{
-		_putchar(n + '0');
-		return;
+		print_number(number / 10);
 	}
 
-	if (n >= 10)
-	{
-		print_number(n / 10);
-	}
-
-	/* print_number(n / 10); */
-	_putchar((n % 10) + '0');
+	_putchar('0' + (number % 10));
 }
