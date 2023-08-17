3f784ba39a25f2cc932bed585997c1f3ec84d608
Maddie <115709272+Maddily@users.noreply.github.com>
2023-08-17 12:00:14 +0300

    Write a fizz-buzz program
    The program prints the numbers from 1 to 100, followed by a new line.
    For multiples of three, it prints Fizz instead of the number,
    for multiples of five, it prints Buzz, and for numbers which are multiples
    of both three and five, it prints FizzBuzz.

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers from 1 to 100
+ * Description: The program prints the numbers, substituting Fizz
+ *		for numbers that are multiples of 3, Buzz for
+ *		multiples of 5 and FizzBuzz for multiples of
+ *		3 and 5
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
