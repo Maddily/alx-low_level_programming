15559c1d6b917a93670fc573ab0ade5d6b7b4158
Maddie <115709272+Maddily@users.noreply.github.com>
2023-08-23 19:51:16 +0300

    Consider string equalities/differences

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,5 +8,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	return (s1[0] - s2[0]);
+	int i;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	if (sizeof(s1) > sizeof(s2))
+		return ((int)(s1[i]));
+	else if (sizeof(s1) < sizeof(s2))
+		return ((int)(s2[i]));
+	else
+		return (0);
 }
