1413eb8f122de6b9735d4b056c0a92ed4ac486e6
Maddie <115709272+Maddily@users.noreply.github.com>
2023-08-23 19:22:24 +0300

    Change how extra characters are dealt with

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		if (i >= 48 && i <= 90)
-			dest[i] = 0;
-		else
-			dest[i] = src[i];
+		dest[i] = src[i];
 	}
+	for (; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
