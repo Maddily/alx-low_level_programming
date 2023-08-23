243f52253d28991c7fcad3431aed486f7a2beb1f
Maddie <115709272+Maddily@users.noreply.github.com>
2023-08-23 20:54:23 +0300

    Fix indenting

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,7 +18,7 @@ char *cap_string(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
-				str[i] -= 32;
+			str[i] -= 32;
 		else
 		{
 			prev = str[i - 1];
