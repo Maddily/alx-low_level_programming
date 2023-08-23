ff6d7c13981dad4683d06734405355248c487625
Maddie <115709272+Maddily@users.noreply.github.com>
2023-08-23 12:50:01 +0300

    Write a function to capitalize words in a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#define CONDITION	((prev == ' ' || prev == ',' || prev == ';' || \
+			prev == '.' || prev == '!' || prev == '?' || \
+			prev == '\t' || prev == '(' || prev == ')' || \
+			prev == '{' || prev == '}' || prev == '\n' || \
+			prev == '"' || prev == '\0') && (str[i] >= 'a' && str[i] <= 'z'))
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @str: A string
+ * Return: The string, capitalized
+ */
+char *cap_string(char *str)
+{
+	int i;
+	char prev;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+		}
+		else
+		{
+			prev = str[i - 1];
+			if (CONDITION)
+				str[i] -= 32;
+		}
+		if (str[i] == '\t')
+			str[i] = ' ';
+	}
+	return (str);
+}
