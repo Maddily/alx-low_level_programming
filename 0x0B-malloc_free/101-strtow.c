84a02bfed40becf89e58adf004af8d8571989f84
Maddie <115709272+Maddily@users.noreply.github.com>
2023-09-05 15:22:33 +0300

    Update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -18,7 +18,6 @@ int findLength(char *str)
 
 /**
  * addWord - Adds words to array of words arr
- * @words: Word count
  * @i: index
  * @j: index
  * @k: index
@@ -27,38 +26,35 @@ int findLength(char *str)
  * @str: A pointer to the string to be split into words
  * @arr: A pointer to an array of pointers
  */
-void addWord(int words, int i, int j, int k, int l, int wordLen, char *str,
+void addWord(int i, int j, int k, int l, int wordLen, char *str,
 		char **arr)
 {
-	for (i = 0; i < words; i++)
+	while (str[i] != '\0')
 	{
-		wordLen = 0;
+		while (str[i] == ' ')
+			i++;
 
-		for (k = k; str[k] != '\0'; k++)
+		if (str[i] != '\0')
 		{
-			if (str[k] == ' ')
-				continue;
-			else
+			wordLen = 0;
+			j = i;
+
+			while (str[j] != ' ' && str[j] != '\0')
 			{
 				wordLen++;
-
-				if (str[k + 1] == ' ')
-					break;
+				j++;
 			}
-		}
 
-		if (wordLen > 0)
-		{
-			l = k - wordLen + 1;
+			arr[k] = malloc((wordLen + 1) * sizeof(char));
+
+			if (arr[k] == NULL)
+				exit(1);
+
+			for (l = 0; l < wordLen; l++)
+				arr[k][l] = str[i++];
+
+			arr[k][l] = '\0';
 			k++;
-			arr[i] = malloc((wordLen + 1) * sizeof(char));
-
-			for (j = 0; j < wordLen; j++)
-			{
-				arr[i][j] = str[l++];
-			}
-
-			arr[i][j] = '\0';
 		}
 	}
 }
@@ -70,22 +66,25 @@ void addWord(int words, int i, int j, int k, int l, int wordLen, char *str,
  */
 char **strtow(char *str)
 {
-	int i, j, k, l, words = 0, wordLen;
+	int i, j, k, l, words = 0, wordLen = 0;
 	char **arr;
 
 	if (str == NULL || findLength(str) == 0)
 		return (NULL);
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i] != '\0')
 	{
-		while (str[i] != ' ' && str[i] != '\0')
-		{
+		while (str[i] == ' ')
 			i++;
-			if (str[i] == ' ' || str[i] == '\0')
-				words++;
+
+		if (str[i] != '\0')
+		{
+			words++;
+
+			while (str[i] != ' ' && str[i] != '\0')
+				i++;
 		}
 	}
-
 	if (words == 0)
 		return (NULL);
 
@@ -94,11 +93,11 @@ char **strtow(char *str)
 	if (arr == NULL)
 		return (NULL);
 
-	j = 0, k = 0, l = 0, wordLen = 0;
+	i = 0, j = 0, k = 0, l = 0;
 
-	addWord(words, i, j, k, l, wordLen, str, arr);
+	addWord(i, j, k, l, wordLen, str, arr);
 
-	arr[i] = NULL;
+	arr[words] = NULL;
 
 	return (arr);
 }
