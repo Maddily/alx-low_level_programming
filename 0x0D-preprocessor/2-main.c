68486a971ae5d43c79fe8e95c6ad795b25823df4
Maddie <115709272+Maddily@users.noreply.github.com>
2023-09-11 10:13:48 +0300

    Document main and add a space after return

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - Prints the name of the file
+ * Return: 0
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
 
-	return(0);
+	return (0);
 }
