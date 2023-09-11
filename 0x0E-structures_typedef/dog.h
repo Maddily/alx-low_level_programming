936cc1b1bdd7d5d7b03112cba37193fc0040411c
Maddie <115709272+Maddily@users.noreply.github.com>
2023-09-11 13:52:15 +0300

    Define a struct containing a dog's data

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_H
+
+#define HEADER_H
+
+/**
+ * struct dog - Data about a dog
+ * @name: A dog's name
+ * @age: A dog's age
+ * @owner: The name of a dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
