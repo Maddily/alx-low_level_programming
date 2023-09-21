8b588264555518c3e97f6df18417399238f6bd0b
Maddie <115709272+Maddily@users.noreply.github.com>
2023-09-21 12:12:49 +0300

    Use printf

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_list - Prints all of the elements of a `list_t` list.
@@ -7,37 +8,19 @@
  */
 size_t print_list(const list_t *h)
 {
-	int i = 0, j;
-	size_t n = 0, len;
+	size_t n = 0;
 	const list_t *current = h;
-	char null_str[] = "[0] (nil)";
-	char len_str[10];
 
 	while (current != NULL)
 	{
-		i = 0;
 		if (current->str == NULL)
 		{
-			for (j = 0; null_str[j] != '\0'; j++)
-				_putchar(null_str[j]);
+			printf("%s\n", "[0] (nil)");
 		}
 		else
 		{
-			len = current->len;
-			while (len > 0)
-			{
-				len_str[i++] = (len % 10) + '0';
-				len /= 10;
-			}
-			_putchar('[');
-			for (i = i - 1; i >= 0; i--)
-				_putchar(len_str[i]);
-			_putchar(']');
-			_putchar(' ');
-			for (i = 0; (current->str)[i] != '\0'; i++)
-				_putchar((current->str)[i]);
+			printf("[%u] %s\n", current->len, current->str);
 		}
-		_putchar('\n');
 		n++;
 		current = current->next;
 	}
