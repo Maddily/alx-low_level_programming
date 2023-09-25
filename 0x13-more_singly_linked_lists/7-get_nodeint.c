9ae644ca2aab333f64f3bb91229de5a1ed81fa40
Maddie <115709272+Maddily@users.noreply.github.com>
2023-09-25 20:02:25 +0300

    Consider edge cases

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,13 +14,18 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	current = head->next;
+	if (index == 0)
+		return (head);
 
-	while (index > 1)
+	current = head;
+	while (index > 1 && current != NULL)
 	{
 		current = current->next;
 		index--;
 	}
 
-	return (current);
+	if (index > 1 || current == NULL)
+		return (NULL);
+
+	return (current->next);
 }
