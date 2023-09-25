d1a1bcfa68d228f6f968b5bd1f0c62a446bb7182
Maddie <115709272+Maddily@users.noreply.github.com>
2023-09-25 18:23:42 +0300

    Assign *head to current only if *head isn't NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,10 +6,12 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
+	listint_t *current;
 
 	if (head == NULL || *head == NULL)
 		return;
+
+	current = *head;
 	while (*head != NULL)
 	{
 		*head = (*head)->next;
