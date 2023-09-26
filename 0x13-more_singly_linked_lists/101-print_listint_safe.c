ac841779d352624c06daf4022e72fbdbbdad7552
Maddie <115709272+Maddily@users.noreply.github.com>
2023-09-26 12:45:00 +0300

    Write a function that prints a list
    The function detects loops inside the list.

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint_safe - Prints a list.
+ * @head: A pointer to the first node in the list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *t, *h;
+	size_t count = 0;
+
+	t = h = head;
+
+	while (t != NULL)
+	{
+		printf("[%p] %d\n", (void *)t, t->n);
+		t = t->next;
+		count++;
+		if (h != NULL && h->next != NULL)
+		{
+			h = h->next->next;
+			if (t == h)
+			{
+				printf("[%p] %d\n", (void *)t, t->n);
+				printf("[%p] %d\n", (void *)(t->next), t->next->n);
+				printf("-> [%p] %d\n", (void *)(t->next->next), t->next->next->n);
+				exit(98);
+			}
+		}
+	}
+
+	return (count);
+}
