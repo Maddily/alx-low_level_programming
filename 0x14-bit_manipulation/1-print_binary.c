e09c00599a2812948ad8cfe297de427996b61910
Maddie <115709272+Maddily@users.noreply.github.com>
2023-09-28 12:35:25 +0300

    Write a function to print base 2 num from base 10

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of a number
+ * @n: The number to be converted to base 2 and printed
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int i;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	i = 1;
+	while (i <= n)
+		i *= 2;
+
+	i = i >> 1;
+
+	while (i > 0)
+	{
+		if (n >= i)
+		{
+			_putchar('1');
+			n -= i;
+		}
+		else
+			_putchar('0');
+
+		i = i >> 1;
+	}
+}
