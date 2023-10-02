f2d4e62c8d1afc23220d4402dc959c742de25e60
Maddie <115709272+Maddily@users.noreply.github.com>
2023-10-02 20:42:47 +0300

    Add function prototypes and include header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <elf.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
@@ -13,5 +14,9 @@ int append_text_to_file(const char *filename, char *text_content);
 int _strlen(char *s);
 void closeAndCheck(int fdFrom, int fdTo);
 void exit99(int fdFrom, int fdTo, char *fileTo);
+void displayMagic(Elf64_Ehdr *elfHeader);
+char *displayType(Elf64_Ehdr *elfHeader);
+char *displayOsAbi(unsigned char osAbi);
+void displayFields(Elf64_Ehdr *elfHeader);
 
 #endif
