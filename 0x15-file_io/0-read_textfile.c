07bd405df92ba62924d4f1edf70033f98ebbcad7
Maddie <115709272+Maddily@users.noreply.github.com>
2023-10-02 11:36:47 +0300

    Write a function that reads and prints text
    The function reads from a file and prints to stdout.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and
+ * prints it to the POSIX standard output.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the function reads and prints.
+ *
+ * Return: The actual number of letters the function could read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+		return (0);
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1)
+		return (0);
+
+	free(buffer);
+	return (bytes_written);
+}
