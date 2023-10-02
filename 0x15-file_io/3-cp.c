7f803f3593dd431ba770bc12abcbbc9ddb791db2
Maddie <115709272+Maddily@users.noreply.github.com>
2023-10-02 16:10:04 +0300

    Rename variables

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,7 @@
  */
 int main(int argc, char **argv)
 {
-	int fd_from, fd_to, bytes_read, bytes_written;
+	int fdFrom = -1, fdTo = -1, bytesRead, bytesWritten;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -18,69 +18,67 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (access(argv[1], F_OK) == -1)
+	fdFrom = open(argv[1], O_RDONLY);
+	if (access(argv[1], F_OK) == -1 || fdFrom == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		closeAndCheck(fdFrom, fdTo);
 		exit(98);
 	}
 	if (access(argv[2], F_OK) != -1)
-		fd_to = open(argv[2], O_TRUNC | O_WRONLY);
+		fdTo = open(argv[2], O_TRUNC | O_WRONLY);
 	else
-		fd_to = open(argv[2], O_CREAT | O_WRONLY, 0664);
-	if (fd_to == -1)
-		exit_99(fd_from, fd_to, argv[2]);
-	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
+		fdTo = open(argv[2], O_CREAT | O_WRONLY, 0664);
+	if (fdTo == -1)
+		exit99(fdFrom, fdTo, argv[2]);
+	while ((bytesRead = read(fdFrom, buffer, sizeof(buffer))) > 0)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
-			exit_99(fd_from, fd_to, argv[2]);
+		bytesWritten = write(fdTo, buffer, bytesRead);
+		if (bytesWritten == -1)
+			exit99(fdFrom, fdTo, argv[2]);
 	}
-	if (bytes_read == -1)
+	if (bytesRead == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close_and_check(fd_from, fd_to);
+		closeAndCheck(fdFrom, fdTo);
 		exit(98);
 	}
-	close_and_check(fd_from, fd_to);
+	closeAndCheck(fdFrom, fdTo);
 	return (0);
 }
 
 /**
- * exit_99 - Exits with a code of 99 if can't write to a file
+ * exit99 - Exits with a code of 99 if can't write to a file
  *
- * @fd_from: First file descriptor
- * @fd_to: Second file descriptor
- * @file_to: The name of the file
+ * @fdFrom: First file descriptor
+ * @fdTo: Second file descriptor
+ * @fileTo: The name of the file
  */
-void exit_99(int fd_from, int fd_to, char *file_to)
+void exit99(int fdFrom, int fdTo, char *fileTo)
 {
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-	close_and_check(fd_from, fd_to);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fileTo);
+	closeAndCheck(fdFrom, fdTo);
 	exit(99);
 }
 
 /**
- * close_and_check - Closes file descriptors and checks if it was successful
+ * closeAndCheck - Closes file descriptors and checks if it was successful
  *
- * @fd_from: First file descriptor
- * @fd_to: Second file descriptor
+ * @fdFrom: First file descriptor
+ * @fdTo: Second file descriptor
  */
-void close_and_check(int fd_from, int fd_to)
+void closeAndCheck(int fdFrom, int fdTo)
 {
-	int close_from, close_to;
+	int closeFrom, closeTo;
 
-	close_from = close(fd_from);
-	close_to = close(fd_to);
-	if (close_from == -1 || close_to == -1)
+	if (fdFrom != -1)
+		closeFrom = close(fdFrom);
+	if (fdTo != -1)
+		closeTo = close(fdTo);
+	if (closeFrom == -1 || closeTo == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
-				(close_from == -1) ? fd_from : fd_to);
+				(closeFrom == -1) ? fdFrom : fdTo);
 		exit(100);
 	}
 }
