b28b8832dcd6522b1f8525d5463542b1952e63fa
Maddie <115709272+Maddily@users.noreply.github.com>
2023-11-09 09:17:32 +0200

    Find the number of elements in a linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Finds the number of elements in a linked list
+ *
+ * @h: A pointer to the first node in the list
+ *
+ * Return: The number of elements in the list
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *c;
+	int n = 0;
+
+	if (h == NULL)
+		return (0);
+
+	c = h;
+
+	while (c != NULL)
+	{
+		n++;
+		c = c->next;
+	}
+
+	return (n);
+}
