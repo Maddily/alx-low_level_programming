98bfbe0a5b2e0f9b02abf571abc25c3936785f7f
Maddie <115709272+Maddily@users.noreply.github.com>
2023-11-09 10:44:04 +0200

    Find a node at a given position in a list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Finds the nth node in a list
+ *
+ * @head: A pointer to the first node in the list
+ * @index: The position of the node to be located
+ *
+ * Return: A pointer to the node to be found
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *current;
+
+	if (head == NULL)
+		return (NULL);
+
+	current = head;
+	while (current != NULL)
+	{
+		if (i == index)
+			return (current);
+		current = current->next;
+		i++;
+	}
+
+	return (NULL);
+}
