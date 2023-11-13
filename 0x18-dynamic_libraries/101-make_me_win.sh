ea3b60c8b19b3782ba4b21a539fd96b79af0c54d
Maddie <115709272+Maddily@users.noreply.github.com>
2023-11-13 16:00:52 +0200

    Update link

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Maddily/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libwin.so
+wget -P /tmp https://github.com/Maddily/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
 export LD_PRELOAD=/tmp/libwin.so
