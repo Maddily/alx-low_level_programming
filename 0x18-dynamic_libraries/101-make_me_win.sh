b68aa1b269bacd4db029d129afec98ce5ab3afbf
Maddie <115709272+Maddily@users.noreply.github.com>
2023-11-13 14:33:18 +0200

    Group the commands

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo 'int rand(void) { return 9; } int system(const char *command) { return 0; }' | gcc -fPIC -shared -o /tmp/libwin.so -xc -
-export LD_PRELOAD=/tmp/libwin.so
+echo 'int rand(void) { return 9; } int system(const char *command) { return 0; }' | gcc -fPIC -shared -o /tmp/libwin.so -xc - && export LD_PRELOAD=/tmp/libwin.so
