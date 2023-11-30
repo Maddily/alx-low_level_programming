79b4cb3f86cb73b80c7cefee98b41cfc04e6fb84
Maddie <115709272+Maddily@users.noreply.github.com>
2023-11-30 19:37:15 +0200

    Update handling when hash table is empty

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -12,31 +12,26 @@ void hash_table_print(const hash_table_t *ht)
 	size_t flag = 0;
 
 	if (!ht)
+		return;
+	i = 0;
+	printf("{");
+	while (i < ht->size)
 	{
-		printf("{}\n");
-	}
-	else
-	{
-		i = 0;
-		printf("{");
-		while (i < ht->size)
+		if (ht->array[i])
 		{
-			if (ht->array[i])
+			if (flag)
+				printf(", ");
+			current = ht->array[i];
+			while (current)
 			{
-				if (flag)
+				printf("'%s': '%s'", current->key, current->value);
+				current = current->next;
+				if (current)
 					printf(", ");
-				current = ht->array[i];
-				while (current)
-				{
-					printf("'%s': '%s'", current->key, current->value);
-					current = current->next;
-					if (current)
-						printf(", ");
-				}
-				flag = 1;
 			}
-			i++;
+			flag = 1;
 		}
-		printf("}\n");
+		i++;
 	}
+	printf("}\n");
 }
